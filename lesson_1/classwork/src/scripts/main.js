328954d1389ba62ebb534c419ca76d1901edbe78
Serhii_Terletckiy <beinght@gmail.com>
2022-02-01 13:58:28 +0200

    --- Lesson 1 --- - Homework and classwork was added

---
+++ lesson_1/classwork/src/scripts/main.js
@@ -0,0 +1,74 @@
+// TASK_1
+// let a_1 = Math.random();
+// let a_2 = Math.random();
+// let a_3 = Math.random();
+// let a_4 = Math.random();
+// let a_5 = Math.random();
+// let a_6 = Math.random();
+// let a_7 = Math.random();
+// let a_8 = Math.random();
+// let a_9 = Math.random();
+// let a_10 = Math.random();
+// let result = 0;
+//
+// let array = [a_1, a_2, a_3, a_4, a_5, a_6, a_7, a_8, a_9, a_10];
+// for (let i = 0; i < array.length; i++) {
+//     result += array[i];
+// }
+// alert(result)
+
+
+// TASK 2-4
+// let book_1 =
+//     {
+//         name: 'name_1',
+//         number_of_page: Math.random(),
+//         genre: 'genre_1123113',
+//     }
+// let book_2 =
+//     {
+//         name: 'name_2',
+//         number_of_page: Math.random(),
+//         genre: 'genre_5',
+//         authors: 'name_112414'
+//     }
+// let book_3 =
+//     {
+//         name: 'name_3',
+//         number_of_page: Math.random(),
+//         genre: 'genre_121',
+//         authors: 'name_1'
+//     }
+// let book_4 =
+//     {
+//         name: 'name_4',
+//         number_of_page: Math.random(),
+//         genre: 'genre_153',
+//         authors: 'name_123'
+//     }
+//
+// let books = [book_1,book_2,book_3,book_4]
+// console.log(books[0])
+// console.log(books[1])
+// console.log(books[2])
+// console.log(books[3])
+
+
+// TASK 5
+// let h = 23;
+// let w = 10;
+// let s = (h*w)/2;
+
+// TASK 6
+//const PI = 3.14
+//let cil_h = 10;
+//let cil_d = 4;
+//let cil_v = cil_h * (PI * 2 * ((cil_d / 2) ** 2));
+//alert(`Циліндр має об'єм ${cil_v} м3`)
+
+
+//TASK 7
+// let n = 3;
+// let m = 4;
+// let l = (Math.pow(n,2) + Math.pow(m,2)) ** (0.5);
+// alert(`Гіпотенуза трикутника = ${l}`)
---
+++ lesson_1/homework/javascript/main.js
@@ -0,0 +1,168 @@
+// {
+//     let hello = 'hello';
+//     let owu = 'owu';
+//     let com = 'com';
+//     let ua = 'ua';
+//     let number = 1;
+//     let number_2 = 10;
+//     let almostTh = -999;
+//     let oneTwoThree = 123;
+//     let PI = 3.14;
+//     let twoPseventh = 2.7;
+//     let sixth = 16;
+//     let boolean_t = true;
+//     let boolean_f = false;
+//
+//     console.log(hello)
+//     console.log(owu)
+//     console.log(com)
+//     console.log(ua)
+//     console.log(number)
+//     console.log(number_2)
+//     console.log(almostTh)
+//     console.log(oneTwoThree)
+//     console.log(PI)
+//     console.log(twoPseventh)
+//     console.log(sixth)
+//     console.log(boolean_t)
+//     console.log(boolean_f)
+//
+//     alert(hello)
+//     alert(owu)
+//     alert(com)
+//     alert(ua)
+//     alert(number)
+//     alert(number_2)
+//     alert(almostTh)
+//     alert(oneTwoThree)
+//     alert(PI)
+//     alert(twoPseventh)
+//     alert(sixth)
+//     alert(boolean_t)
+//     alert(boolean_f)
+//
+//     document.write(hello)
+//     document.write(owu)
+//     document.write(com)
+//     document.write(ua)
+//     document.write(number)
+//     document.write(number_2)
+//     document.write(almostTh)
+//     document.write(oneTwoThree)
+//     document.write(PI)
+//     document.write(twoPseventh)
+//     document.write(sixth)
+//     document.write(boolean_t)
+//     document.write(boolean_f)
+//
+//
+//     hello = 'not hello';
+//     owu = 'UwU';
+//     com = 'ua';
+//     ua = 'UA!';
+//     number = 333;
+//     number_2 = 666;
+//     almostTh = 999;
+//     oneTwoThree = 321;
+//     PI = 3;
+//     twoPseventh = 22.7;
+//     sixth = 177;
+//     boolean_t = false;
+//     boolean_f = true;
+//
+//     console.log(hello)
+//     console.log(owu)
+//     console.log(com)
+//     console.log(ua)
+//     console.log(number)
+//     console.log(number_2)
+//     console.log(almostTh)
+//     console.log(oneTwoThree)
+//     console.log(PI)
+//     console.log(twoPseventh)
+//     console.log(sixth)
+//     console.log(boolean_t)
+//     console.log(boolean_f)
+//
+//     alert(hello)
+//     alert(owu)
+//     alert(com)
+//     alert(ua)
+//     alert(number)
+//     alert(number_2)
+//     alert(almostTh)
+//     alert(oneTwoThree)
+//     alert(PI)
+//     alert(twoPseventh)
+//     alert(sixth)
+//     alert(boolean_t)
+//     alert(boolean_f)
+//
+//     document.write(hello)
+//     document.write(owu)
+//     document.write(com)
+//     document.write(ua)
+//     document.write(number)
+//     document.write(number_2)
+//     document.write(almostTh)
+//     document.write(oneTwoThree)
+//     document.write(PI)
+//     document.write(twoPseventh)
+//     document.write(sixth)
+//     document.write(boolean_t)
+//     document.write(boolean_f)
+// }
+
+
+// let firstName = 'Serhiy';
+// let middleName = 'Andriyovich';
+// let lastName = 'Terletskiy';
+// let person = firstName + middleName + lastName;
+//
+// firstName = prompt('Твоє ім\'я?', 'Serhii');
+// middleName = prompt('Твоє по-батькові?', 'Andriyovich');
+// let age = prompt('Твій вік?', 18);
+//
+// alert(`Вітаю ${firstName} ${middleName}. Тобі ${age} років`);
+
+
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+
+// 5 < 6  -> true
+// 5 > 6 -> false
+// 5 == 6 -> false
+// 5 === 6 -> false
+// 10 >= 10 -> true
+// 10 <= 10 -> true
+// 10 > 10 -> false
+// 10 < 10 -> false
+// 10 != 10 -> false
+// 123 === '123' -> false
+// 123 == '123' -> true
+
+let str = '20';
+let a = 5;
+document.write(str + a + "<br/>")
+document.write(str - a + "<br/>")
+document.write(str * '2' + "<br/>")
+document.write(str / 2 + "<br/>")
+//Всі оператори крім "+" працюють тільки як арифметичні в даному прикладі. "+" же, може означати конкатинацію.
+
+
+
+
+
+
+
+
+
+
+
