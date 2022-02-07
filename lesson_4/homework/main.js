327665f601f7e53ec135722b8e2ddde4348d2d9d
Serhii_Terletckiy <beinght@gmail.com>
2022-02-07 21:51:46 +0200

    --- Lesson 4 --- 1.Homework was added

---
+++ lesson_4/homework/main.js
@@ -0,0 +1,71 @@
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами "а" і "б"
+function rectangle_area(a, b) {
+    return a * b;
+}
+
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом "r"
+function circle_area(r) {
+    return 2 * Math.PI * (r ** 2);
+}
+
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою "h", та радіусом "r"
+function cylinder_area(h, r) {
+    return 2 * Math.PI * (r ** 2) + h * (2 * r * Math.PI);
+}
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+function iteration(lst) {
+    for (let i = 0; i < lst.length; i++) {
+        console.log(lst[i]);
+
+    }
+}
+
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+function paragraphWtext(text) {
+    document.write(`<h1>${text}</h1>>`)
+}
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+function simpleList(text) {
+    document.write(`<ul>`)
+    for (let i = 0; i < 3; i++) {
+        document.write(`<li>${text}</li>>`);
+    }
+    document.write(`</ul>`)
+}
+
+
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+function moreComplexList(text, li_count) {
+    document.write(`<ul>`)
+    for (let i = 0; i < li_count; i++) {
+        document.write(`<li>${text}</li>>`);
+    }
+    document.write(`</ul>`)
+}
+
+
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+function evenMoreComplexList(lst) {
+    document.write(`<ul>`)
+    for (const elem of lst) {
+        document.write(`<li>${elem}</li>>`);
+    }
+    document.write(`</ul>`)
+}
+
+// - створити функцію яка приймає масив об'єктів з наступними полями id, name, age, та виводить їх в документ. Для кожного об'єкту окремий блок.
+function objectList(lst) {
+    document.write(`<div>`)
+    for (const lstElement of lst) {
+        document.write(`<ul>`)
+        document.write(`<li>${lstElement.id}</li>`)
+        document.write(`<li>${lstElement.name}</li>`)
+        document.write(`<li>${lstElement.age}</li>`)
+        document.write(`</ul>`)
+    }
+    document.write(`</div>`)
+}
